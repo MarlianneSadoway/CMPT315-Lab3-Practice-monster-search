4b0a3dbb4bebf93e51fc84c73ae4732b5c063535
MarlianneSadoway <sadowaym7@mymacewan.ca>
2024-01-25 13:21:22 -0700

    create cards of monsters and functional search bar

--- src/App.js
+++ src/App.js
@@ -1,25 +1,50 @@
-import logo from './logo.svg';
 import './App.css';
 
+import CardList from "./components/cardlist/cardlist.component";
+import { SearchBar } from "./components/searchbar/searchbar.component";
+
+import React, { useState, useEffect } from "react";
+  
 function App() {
+  const [monsters, setMonsters] = useState([]);
+  const [filteredMonsters, setFilteredMonsters] = useState([]);
+  const [searchInput, setSearchInput] = useState("");
+
+  useEffect(() => {
+    const fetchMonsters = async () => {
+      const response = await fetch("https://jsonplaceholder.typicode.com/users");
+      const monsters = await response.json();
+      setMonsters(monsters)
+    };
+    fetchMonsters();
+  }, []); // empty array of dependencies, so runs once on initially load 
+
+  useEffect(() => {
+    let filtered = [];
+    if (searchInput === "") {
+    filtered = monsters
+    } else {
+    filtered = monsters.filter(monster =>
+    monster.name.toLowerCase().includes(searchInput.toLowerCase())
+    );
+    }
+    setFilteredMonsters(filtered);
+  }, [monsters, searchInput]); // renders when dependencies change
+
+  const handleInput = e => {
+    setSearchInput(e.target.value)
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Monster Rolodex</h1>
+      <SearchBar
+        placeholder='Search Monster'
+        handleInput={handleInput}
+      />
+      <CardList monsters={filteredMonsters} />
     </div>
-  );
+    );
 }
 
 export default App;
